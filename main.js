73c7845620cbb7f5020663473133eb87d61e7f9d
Camila Cristina <criscamila09@gmail.com>
2022-07-28 21:23:37 -0300

    mobile shopping-cart-page

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ function openMenu() {
 
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('sp-expanded')
+}
+
+function openShoppingCart() {
+  document.body.classList.add('sp-expanded')
+}
+
+function closeShoppingCart() {
+  document.body.classList.remove('sp-expanded')
 }
